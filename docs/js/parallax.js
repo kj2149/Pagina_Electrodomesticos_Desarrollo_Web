e79c94b7622035fc8aa1a379eb8c81e913604f7b
camilo sierra <camilo.sierra2149@gmail.com>
2021-01-08 01:19:55 -0500

    comentario en js

--- docs/js/parallax.js
+++ docs/js/parallax.js
@@ -1,3 +1,3 @@
 
-    new universalParallax[].init();
-<script src="node_modules/universal-parallax/dist/universal-parallax.min.js"></script>
+ /*   new universalParallax[].init();
+<script src="node_modules/universal-parallax/dist/universal-parallax.min.js"></script>*/

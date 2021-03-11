170b0d04eb89a990b28b8e8844581601eaf92af2
camilo sierra <camilo.sierra2149@gmail.com>
2021-03-11 11:42:21 -0500

    css de registro

--- Proyecto_Base_Datos/vistas/login.php
+++ Proyecto_Base_Datos/vistas/login.php
@@ -4,9 +4,16 @@
         <script src="https://kit.fontawesome.com/712575d4a5.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="../css/pie_pag_login.css" type="text/css" /><style type="text/css">._css3m{display:none}</style>
         <link rel="stylesheet" href="../css/iniciosecion.css" type="text/css" /><style type="text/css">._css3m{display:none}</style>
+        <link rel="stylesheet" href="../css/estilos3.css" type="text/css" /><style type="text/css">._css3m {display: none;}}</style>
+        <link rel="stylesheet" type="text/css" href="../css/estilos3.css">
+        <link rel="stylesheet" type="text/css" href="../css/menu_desplegable_filtro.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="keyswords" content="html5, css3">
+         <!-- <link rel="stylesheet" type="text/css" href="../css/icono.css">  -->   
+        <!--tipografia Josefin Sans-->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
         
             <!--IMPLEMENTANDO BOOTSTRAP - Ajax -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,6 +22,196 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <title>Unlimited Tecnhology-Iniciar Sesion</title>
     </head>  
+    <!--***********************-------   HEADER  -----------*************************************************-->
+	<header>
+           <div class="container-head">
+	  
+      
+     <!--LOGO DE LA EMPRESA-->
+	  
+		<img class="logo" src="../imagenes/2.png" alt="logoempresa">	
+	
+     <!--TITULO ULIMITED TECNHOLOGY-->
+
+		  <img class="titulo1" src="../imagenes/titulo.png" alt="UNLIMITED TECNHOLOGY">
+
+     <!--BUSCADOR-BOTONES INICIO , REGISTRAR-->
+
+	 <div class="buscar1">
+			<div class=" barra-btn-buscar">
+			   <input type="" placeholder="Buscar" required>
+			   <div class="btn">
+				<i class="fas fa-search icon"></i>
+			  </div>
+		      </div>
+		     
+        </div>
+
+
+		
+
+     <!--LISTA DE CATEGORIAS-->
+	 
+       <div class="lista-cate"></div>
+		   
+			<div >
+				<input type="checkbox" id="menu_bar">
+				<label class="fa fa-bars fa-2x"  for="menu_bar" ></label>
+				
+				
+				<nav class="menu">
+		
+					<div class="filtro">
+		
+						<a href=""><h3 class="text-white">Celulares</h3></a>
+						<a>36.744 resultados</a>
+		
+						<a href=""><h4 class="text-white">Tiendas oficiales</h4></a>
+						<a href="#"><span>Solo tiendas oficiales</span>
+							<span>(200)</span></a>
+		
+						<a href=""><h4 class="text-white">Marcas</h4></a>
+		
+						<ul>
+							<li><a href="#">Iphone <span>(10.400)</span></a></li>
+							<li><a href="#">Redmi <span>(5.200)</span></a></li>
+							<li><a href="#">Galaxy A <span>(1.200)</span></a></li>
+							<li><a href="#">Galaxy <span>(1.800)</span></a></li>
+							<li><a href="#">P Series <span>(950)</span></a></li>
+							<li><a href="#">Y Series <span>(875)</span></a></li>
+							<li><a href="#">Galaxy S <span>(800)</span></a></li>
+							<li><a href="#">Galaxy J <span>(450)</span></a></li>
+							<li><a href="#">Moto <span>(1.500)</span></a></li>
+		
+							<li><a href="#" data-toggle="modal" data-target="#myModal">Ver todos</a></li>
+						</ul>
+		
+						<a href=""><h4 class="text-white">Modelos</h4></a>
+		
+						<ul>
+							<li><a href="#">Iphone 7 <span>(500)</span></a></li>
+							<li><a href="#">Iphone 7 Plus<span>(546)</span></a></li>
+							<li><a href="#">Iphone 8 Plus <span>(300)</span></a></li>
+							<li><a href="#">Iphone 11 <span>(400)</span></a></li>
+							<li><a href="#">Iphone X <span>(950)</span></a></li>
+		
+							<li><a href="#">Ver todos</a></li>
+						</ul>
+		
+						<a href=""><h4 class="text-white">Capacidades</h4></a>
+		
+						<ul>
+		
+							<li><a href="#">16GB RAM / 256GB ROM <span>(800)</span></a></li>
+							<li><a href="#">8GB RAM / 128GB ROM <span>(750)</span></a></li>
+							<li><a href="#">4GB RAM / 64GB ROM <span>(340)</span></a></li>
+							<li><a href="#">2GB RAM / 32GB ROM<span>(750)</span></a></li>
+							<li><a href="#">1GB RAM / 16GB ROM <span>(100)</span></a></li>
+		
+							<li><a href="#">Ver todos</a></li>
+						</ul>
+		
+						<a href=""><h4 class="text-white">Descuentos</h4></a>
+		
+						<ul>
+		
+							<li><a href="#">Desde 5% OFF  <span>(137)</span></a></li>
+							<li><a href="#">Desde 10% OFF <span>(50)</span></a></li>
+							<li><a href="#">Desde 15% OFF <span>(140)</span></a></li>
+							<li><a href="#">Desde 20% OFF <span>(250)</span></a></li>
+							<li><a href="#">Desde 25% OFF <span>(160)</span></a></li>
+							<li><a href="#">Desde 30% OFF <span>(161)</span></a></li>
+		
+							<li><a href="#">Ver todos</a></li>
+						</ul>
+		
+						<a href=""><h4 class="text-white">Otras caracteristicas</h4></a>
+		
+						<ul>
+		
+							<li><a href="#">Es Dual SIM <span>(139)</span></a></li>
+							<li><a href="#">Resistente al agua <span>(80)</span></a></li>
+							<li><a href="#">Reconocimiento facial <span>(540)</span></a></li>
+							<li><a href="#">Lector de huella digital <span>(50)</span></a></li>
+		
+							<li><a href="#">Ver todos</a></li>
+						</ul>				
+		
+		
+						<a href=""><h4 class="text-white">Red</h4></a>
+		
+						<ul>
+							<li><a href="#">2G <span>(10.400)</span></a></li>
+							<li><a href="#">3G <span>(5.200)</span></a></li>
+							<li><a href="#">4G <span>(1.200)</span></a></li>
+							<li><a href="#">5G <span>(1.800)</span></a></li>
+							<li><a href="#">4G/LTE <span>(950)</span></a></li>
+		
+							<li><a href="#" data-toggle="modal" data-target="#myModal">Ver todos</a></li>
+						</ul>
+		
+					</div> 
+		
+				</nav>
+			</div>	
+		<!--Fin del div antes de aside-->
+
+		<ul id="css3menu1" class="topmenu">
+			<li class="topmenu"><a href="inicio.html" target="marco" style="height:28px;line-height:22px;">Inicio</a>
+			</li>
+			<li class="toproot"><a href="#" target="marco"style="height:28px;line-height:22px;"><span>Productos</span></a>
+				<ul>
+					<li><a href="../vista/celulares.html" target="marco">Celulares</a></li>
+					<li><a href="../vista/computadores.html" target="marco">Computadores</a>
+
+					    <ul style="left:150px;">
+							
+					    <li><a href="../vista/combo teclado + mouse.html">Combo teclado + Mouse</a></li>
+						<li><a href="../vista/portatiles.html">Portatiles</a></li>
+						<li><a href="../vista/equipos de escritorio.html">Equipos De Escritorio</a></li>
+						<li><a href="../vista/equipos todo en uno.html">Equipos Todo En Uno</a></li>
+                        </ul>
+						</li>
+				
+					<li><a href="../vista/boards.html" target="marco">Boards</a></li>
+					<li><a href="../vista/tarjetas graficas.html" target="marco">Tarjetas Graficas</a></li>
+					<li><a href="../vista/procesadores.html" target="marco">Procesadores</a></li>
+					<li><a href="../vistas/gabinetes.php" target="marco">Gabinetes</a></li>
+					<li><a href="../vistas/fuentes de poder.php" target="marco">Fuentes De Poder</a></li>
+					<li><a href="../vistas/impresoras.php" target="marco">Impresoras</a>
+					<li><a href="../vistas/herramientas.php" target="marco">herramientas</a>
+			
+
+					<ul style="left:150px;">	
+					    <li><a href="../vistas/componentes.php">Componentes</a></li>
+						<li><a href="../vistas/repuestos.php">Repuestos</a></li>
+						<li><a href="../vistas/cableado.php">Cableado</a></li>
+                        </ul>
+						</li>
+				</ul>
+			</li>
+			<li class="toproot"><a href="#" target="marco"
+					style="height:28px;line-height:22px;"><span>Servicios</span></a>
+				<ul>
+					<li><a href="#" target="marco">Instalaciones</a></li>
+					<li><a href="#" target="marco">Mantenimientos</a></li>
+					<li><a href="#" target="marco">Proyectos</a></li>
+				</ul>
+			</li>
+			
+			<li class="toproot"><a href="#" target="marco" style="height:28px;line-height:22px;"><span>Quienes somos?</span></a>
+				<ul>
+					<li><a href="#" target="marco">Vision & Mision</a></li>
+					<li><a href="#" target="marco">Valores Calidad</a></li>
+					<li><a href="#" target="marco">Que hacemos?</a></li>
+				</ul>
+			</li>
+			<li class="toplast"><a href="#" target="marco" style="height:26px;line-height:22px;">Contactenos</a></li>
+		</ul>
+		
+	   </div>
+	</header>
+<!--**************************** FIN ---- HEADER    *************************************************-->
 
      <body id="fondo2">
      <section>   

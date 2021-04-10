dce40afc4e3abac6bd2bf1c42577aa228b2c9451
Sara-mariana <76223831+Sara-mariana@users.noreply.github.com>
2021-04-10 18:04:23 -0500

    Update terminos y condiciones.php
    Cambios y arreglos

--- Proyecto_Base_Datos/vistas/terminos y condiciones.php
+++ Proyecto_Base_Datos/vistas/terminos y condiciones.php
@@ -33,7 +33,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <title>Unlimited Tecnhology</title>
+  <title>Terminos y Condiciones - Unlimited Tecnhology</title>
 </head>
 
 <body id="parallax" class="text-white">

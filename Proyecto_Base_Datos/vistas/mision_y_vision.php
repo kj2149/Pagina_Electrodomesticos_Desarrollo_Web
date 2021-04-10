7f83be1bb142d954a2859514d1dd3f173d948686
Sara-mariana <76223831+Sara-mariana@users.noreply.github.com>
2021-04-10 17:50:23 -0500

    Update mision_y_vision.php
    cambios  y arreglos

--- Proyecto_Base_Datos/vistas/mision_y_vision.php
+++ Proyecto_Base_Datos/vistas/mision_y_vision.php
@@ -5,7 +5,7 @@
         <link rel="shortcut icon" href="../imagenes/mifavicon.png"/>
 
 	<script src="https://kit.fontawesome.com/712575d4a5.js" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="../css/estilos3.css" type="text/css" />
+	<link rel="stylesheet" href="../css/estilos4.css" type="text/css" />
 	<style type="text/css">
 		._css3m {
 			display: none
@@ -33,7 +33,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	<title>Unlimited Tecnhology</title>
+	<title>¿Quienes somos? - Unlimited Tecnhology</title>
 </head>
 
 <body id="parallax" class="text-white">
@@ -45,12 +45,11 @@
       
      <!--LOGO DE LA EMPRESA-->
 	  
-		<img class="logo" src="../imagenes/2.png" alt="logoempresa">	
+		<a href="../vista/index.html"class="logo"> <img  src="../imagenes/2.png" alt="logoempresa">	</a>
 	
      <!--TITULO ULIMITED TECNHOLOGY-->
 
-		  <img class="titulo1" src="../imagenes/titulo.png" alt="UNLIMITED TECNHOLOGY">
-
+		  <a href="../vista/index.html"class="logo"> <img  src="../imagenes/2.png" alt="logoempresa">	</a>
      <!--BUSCADOR-BOTONES INICIO , REGISTRAR-->
 
 	 <div class="buscar1">
@@ -64,114 +63,6 @@
 		      <a class="registrar1" href="#" > <button type="button" class="bot-log-reg">Registrarse</button></a>
         </div>
 
-
-		
-
-     <!--LISTA DE CATEGORIAS-->
-	 
-       <div class="lista-cate"></div>
-		   
-			<div >
-				<input type="checkbox" id="menu_bar">
-				<label class="fa fa-bars fa-2x"  for="menu_bar" ></label>
-				
-				
-				<nav class="menu">
-		
-					<div class="filtro">
-		
-						<a href=""><h3 class="text-white">Celulares</h3></a>
-						<p>36.744 resultados</p>
-		
-						<a href=""><h4 class="text-white">Tiendas oficiales</h4></a>
-						<a href="#"><span>Solo tiendas oficiales</span>
-							<span>(200)</span></a>
-		
-						<a href=""><h4 class="text-white">Marcas</h4></a>
-		
-						<ul>
-							<li><a href="#">Iphone <span>(10.400)</span></a></li>
-							<li><a href="#">Redmi <span>(5.200)</span></a></li>
-							<li><a href="#">Galaxy A <span>(1.200)</span></a></li>
-							<li><a href="#">Galaxy <span>(1.800)</span></a></li>
-							<li><a href="#">P Series <span>(950)</span></a></li>
-							<li><a href="#">Y Series <span>(875)</span></a></li>
-							<li><a href="#">Galaxy S <span>(800)</span></a></li>
-							<li><a href="#">Galaxy J <span>(450)</span></a></li>
-							<li><a href="#">Moto <span>(1.500)</span></a></li>
-		
-							<li><a href="#" data-toggle="modal" data-target="#myModal">Ver todos</a></li>
-						</ul>
-		
-						<a href=""><h4 class="text-white">Modelos</h4></a>
-		
-						<ul>
-							<li><a href="#">Iphone 7 <span>(500)</span></a></li>
-							<li><a href="#">Iphone 7 Plus<span>(546)</span></a></li>
-							<li><a href="#">Iphone 8 Plus <span>(300)</span></a></li>
-							<li><a href="#">Iphone 11 <span>(400)</span></a></li>
-							<li><a href="#">Iphone X <span>(950)</span></a></li>
-		
-							<li><a href="#">Ver todos</a></li>
-						</ul>
-		
-						<a href=""><h4 class="text-white">Capacidades</h4></a>
-		
-						<ul>
-		
-							<li><a href="#">16GB RAM / 256GB ROM <span>(800)</span></a></li>
-							<li><a href="#">8GB RAM / 128GB ROM <span>(750)</span></a></li>
-							<li><a href="#">4GB RAM / 64GB ROM <span>(340)</span></a></li>
-							<li><a href="#">2GB RAM / 32GB ROM<span>(750)</span></a></li>
-							<li><a href="#">1GB RAM / 16GB ROM <span>(100)</span></a></li>
-		
-							<li><a href="#">Ver todos</a></li>
-						</ul>
-		
-						<a href=""><h4 class="text-white">Descuentos</h4></a>
-		
-						<ul>
-		
-							<li><a href="#">Desde 5% OFF  <span>(137)</span></a></li>
-							<li><a href="#">Desde 10% OFF <span>(50)</span></a></li>
-							<li><a href="#">Desde 15% OFF <span>(140)</span></a></li>
-							<li><a href="#">Desde 20% OFF <span>(250)</span></a></li>
-							<li><a href="#">Desde 25% OFF <span>(160)</span></a></li>
-							<li><a href="#">Desde 30% OFF <span>(161)</span></a></li>
-		
-							<li><a href="#">Ver todos</a></li>
-						</ul>
-		
-						<a href=""><h4 class="text-white">Otras caracteristicas</h4></a>
-		
-						<ul>
-		
-							<li><a href="#">Es Dual SIM <span>(139)</span></a></li>
-							<li><a href="#">Resistente al agua <span>(80)</span></a></li>
-							<li><a href="#">Reconocimiento facial <span>(540)</span></a></li>
-							<li><a href="#">Lector de huella digital <span>(50)</span></a></li>
-		
-							<li><a href="#">Ver todos</a></li>
-						</ul>				
-		
-		
-						<a href=""><h4 class="text-white">Red</h4></a>
-		
-						<ul>
-							<li><a href="#">2G <span>(10.400)</span></a></li>
-							<li><a href="#">3G <span>(5.200)</span></a></li>
-							<li><a href="#">4G <span>(1.200)</span></a></li>
-							<li><a href="#">5G <span>(1.800)</span></a></li>
-							<li><a href="#">4G/LTE <span>(950)</span></a></li>
-		
-							<li><a href="#" data-toggle="modal" data-target="#myModal">Ver todos</a></li>
-						</ul>
-		
-					</div> 
-		
-				</nav>
-			</div>	
-		<!--Fin del div antes de aside-->
 
 		<ul id="css3menu1" class="topmenu">
 			<li class="topmenu"><a href="inicio.html" target="marco" style="height:28px;line-height:22px;">Inicio</a>
@@ -229,27 +120,80 @@
 	   </div>
 	</header>
 
-<div class="especificartermino">
-    <center><h1>MISIÓN</h1></center> <br> <br>
-    <p>
-        Ser una firma reconocida por ofrecer la mejor experi encia en soluciones tecnológicas,
-         de seguridad y de  servicio técnico especializado de la más alta calidad;  
-        con recurso humano, profesional experto en  tecnología de punta, que permita cumplir
-        oportuna mente con los estándares de calidad por encima de  las expectativas que el cliente requiere,
-        para los  diferentes sectores comercial, industrial y educación  
+<!-----------------------------------------------FIN HEADER-------------------------------->
+<div class="container-quienes-somos">
 
-    </p>
+    <div class="mision-vision">
+        <center><h1>MISIÓN</h1></center> <br> <br>
+            <p>
+             Ser una firma reconocida por ofrecer la mejor experi encia en soluciones tecnológicas,
+             de seguridad y de  servicio técnico especializado de la más alta calidad;  
+             con recurso humano, profesional experto en  tecnología de punta, que permita cumplir
+             oportuna mente con los estándares de calidad por encima de  las expectativas que el cliente requiere,
+             para los  diferentes sectores comercial, industrial y educación  
+            </p>
+    </div>
+
+    <div class="mision-vision">
+       <center><h2>VISIÓN</h2></center> <br> <br>
+           <p>
+            Ser una empresa reconocida nacional e internacionalmente, siendo  una de las mejores empresas de Diseño
+            y desarrollo de software, cumpliendo con los estándares de mejora en cuanto a la creación de softwares.
+            De tal manera que se logre el acuerdo mutuo del cliente y nosotros como empresa prestando un buen servicio.
+           </p>
+    </div>
+
+    <div class="objetivo">
+        <center><h2>OBJETIVO (G.S.M. COLOMBIA)</h2></center> <br> <br>
+           <p>
+            Ser en el 2017 el mejor proveedor en la prestación  de soluciones tecnológicas,
+            de seguridad y de  servicio técnico en varios sectores de Bogotá y  Colombia, apoyando al sector Comercial,
+            Indus trial y Educación, para crear y desarrollar infrae structuras mas eficientes para los negocios global izados. 
+           </p>
+    </div>
+
+    <div class="valor">
+      <center><h1>VALORES DE CALIDAD</h1></center> <br> <br>
+       <p>
+        - Responsabilidad individual. 
+        <br>
+        - El mejor servicio y soporte. 
+        <br>
+        - Responsabilidad y bienestar a la comunidad.
+        <br> 
+        - Destrezas flexibles para personalizar la infraestructura. 
+
+       </p>
+    </div>
+
+    <div class="politica">
+        <center><h2>POLITICA DE CALIDAD</h2></center> <br> <br>
+          <p>
+           Unlimited Technology y/o Edilberto Torres Ortiz, Cuenta con  el mejor recurso humano comprometido y satisfecho, 
+           que  trabaja coordinadamente para ofrecer el mejor soporte y de  conocimiento de la tecnología para garantizar 
+           la mejora  continua, la efectividad, la simplicidad de los procesos y la  calidad de los servicios para así 
+           satisfacer las necesidades  de sus clientes, así como a prevenir y reducir los riesgos por  inactividad generados 
+           por la actividad propia de la empresa,  en su plataforma tecnológica. Con base en el Sistema de  Gestión 
+           de Calidad y Ambiental, cumpliendo con los objeti vos, la normatividad y la legislación aplicable.
+
+          </p>
+    </div>
+
+    <div class="objetivo">
+        <center><h1>OBJETIVO DE CALIDAD</h1></center> <br> <br>
+          <p> 
+            Exceder las necesidades y expectativas de nuestros clientes.  
+            Diseñar, Implementar y Mantener sistemas tecnológicos de  información, seguridad y comunicación eficientes. 
+            Desarrollar la cultura de calidad y mejoramiento continuo con  enfoque al cliente.  
+            Mantener un personal competente y comprometido para  cumplir la Misión y Visión empresarial. 
+            Mejorar la productividad, cobertura y la rentabilidad que garan tice el crecimiento de la empresa. 
+ 
+          </p>
+    </div>
+    
 </div>
 
-<div class="especificartermino1">
-   <center><h2>VISIÓN</h2></center> <br> <br>
-   <p>
-    Ser en el 2017 el mejor proveedor en la prestación  de soluciones tecnológicas,
-     de seguridad y de  servicio técnico en varios sectores de Bogotá y  Colombia, apoyando al sector Comercial,
-     Indus trial y Educación, para crear y desarrollar infrae structuras mas eficientes para los negocios global izados. 
 
-   </p>
-</div>
 <!-- FOOTER DE LA PAGINA -->
 
  

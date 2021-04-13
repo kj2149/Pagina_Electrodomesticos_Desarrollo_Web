4f87c5dcd84d35971f1b568369ea4eae11cbd0cb
Sara-mariana <76223831+Sara-mariana@users.noreply.github.com>
2021-04-13 08:16:39 -0500

    Add files via upload

--- Proyecto_Base_Datos/vistas/index.php
+++ Proyecto_Base_Datos/vistas/index.php
@@ -45,26 +45,25 @@
       
      <!--LOGO DE LA EMPRESA-->
 	  
-		<a href="../vista/index.html"class="logo"> <img  src="../imagenes/2.png" alt="logoempresa">	</a>	
+		<a href="../vista/index.html"class="logo"> <img  src="../imagenes/2.png" alt="logoempresa">	</a>
 	
      <!--TITULO ULIMITED TECNHOLOGY-->
 
-		<a href="../vista/index.html" class="titulo1"> <img  src="../imagenes/titulo.png" alt="UNLIMITED TECNHOLOGY"> </a>
+	 	<a href="../vista/index.html" class="titulo1"> <img  src="../imagenes/titulo.png" alt="UNLIMITED TECNHOLOGY"> </a>
 
      <!--BUSCADOR-BOTONES INICIO , REGISTRAR-->
 
 	 <div class="buscar1">
 			<div class=" barra-btn-buscar">
-			   <input type="text" placeholder="Buscar" required>
-			   <div class="btn">
+			  <input type="text" placeholder="Buscar" required>
+			  <div class="btn">
 				<i class="fas fa-search icon"></i>
 			  </div>
 		      </div>
-		      <a class="iniciar1" href="login.php" ><button type="button" class="bot-log-reg">Acceder</button></a> 
-		      <a class="registrar1" href="registro.php" > <button type="button" class="bot-log-reg">Registrarse</button></a>
+		      <a class="iniciar1" href="../vista/login.html" ><button type="button" class="bot-log-reg">Acceder</button></a> 
+		      <a class="registrar1" href="#" > <button type="button" class="bot-log-reg">Registrarse</button></a>
         </div>
-
- <!--Menu Header-->
+    <!--Menu Header-->
 		<ul id="css3menu1" class="topmenu">
 			<li class="topmenu"><a href="inicio.html" target="marco" style="height:28px;line-height:22px;">Inicio</a>
 			</li>
@@ -85,43 +84,32 @@
 					<li><a href="../vista/boards.html" target="marco">Boards</a></li>
 					<li><a href="../vista/tarjetas graficas.html" target="marco">Tarjetas Graficas</a></li>
 					<li><a href="../vista/procesadores.html" target="marco">Procesadores</a></li>
-					<li><a href="../vistas/gabinetes.php" target="marco">Gabinetes</a></li>
-					<li><a href="../vistas/fuentes de poder.php" target="marco">Fuentes De Poder</a></li>
-					<li><a href="../vistas/impresoras.php" target="marco">Impresoras</a>
-					<li><a href="../vistas/herramientas.php" target="marco">herramientas</a>
+					<li><a href="../vistas/gabinetes.html" target="marco">Gabinetes</a></li>
+					<li><a href="../vistas/fuentes de poder.html" target="marco">Fuentes De Poder</a></li>
+					<li><a href="../vistas/impresoras.html" target="marco">Impresoras</a>
+					<li><a href="../vistas/herramientas.html" target="marco">herramientas</a>
 			
 
 					<ul style="left:150px;">	
-					    <li><a href="../vistas/componentes.php">Componentes</a></li>
-						<li><a href="../vistas/repuestos.php">Repuestos</a></li>
-						<li><a href="../vistas/cableado.php">Cableado</a></li>
+					    <li><a href="../vistas/componentes.html">Componentes</a></li>
+						<li><a href="../vistas/repuestos.html">Repuestos</a></li>
+						<li><a href="../vistas/cableado.html">Cableado</a></li>
                         </ul>
 						</li>
 				</ul>
 			</li>
-			<li class="toproot"><a href="#" target="marco"
-					style="height:28px;line-height:22px;"><span>Servicios</span></a>
-				<ul>
-					<li><a href="#" target="marco">Instalaciones</a></li>
-					<li><a href="#" target="marco">Mantenimientos</a></li>
-					<li><a href="#" target="marco">Proyectos</a></li>
-				</ul>
-			</li>
+			<li class="toproot"><a href="servicio.html" target="marco"style="height:28px;line-height:22px;"><span>Servicios</span></a></li>
 			
-			<li class="toproot"><a href="#" target="marco" style="height:28px;line-height:22px;"><span>Quienes somos?</span></a>
-				<ul>
-					<li><a href="#" target="marco">Vision & Mision</a></li>
-					<li><a href="#" target="marco">Valores Calidad</a></li>
-					<li><a href="#" target="marco">Que hacemos?</a></li>
-				</ul>
+			<li class="toproot"><a href="quienes somos.html" target="marco" style="height:28px;line-height:22px;"><span>Quienes somos?</span></a>
 			</li>
-			<li class="toplast"><a href="#" target="marco" style="height:26px;line-height:22px;">Contactenos</a></li>
+			<li class="toplast"><a href="contactenos.html" target="marco" style="height:26px;line-height:22px;">Contactenos</a></li>
 		</ul>
 		
 	   </div>
 	</header>
 <!--**************************** FIN ---- HEADER    *************************************************-->
-	<!--LISTA DE CATEGORIAS-->
+	
+<!--LISTA DE CATEGORIAS-->
 	<aside class="lista-cate">
 		   
 			
@@ -234,7 +222,8 @@
             </nav>				
 </aside>	 
       
-	<div>
+	<!--Fin del div antes de aside-->
+
 		<div class="slider">
 			<ul>
 				<li><img src="../imagenes/s3.png" alt=""></li>
@@ -245,9 +234,8 @@
 		</div>
 	
   
-<!--/////////////          contenido         //////////////-->
-
-	<section class="container-index-contenido">
+<!--/////////////      CONTENIDO       //////////////-->
+<section class="container-index-contenido">
 		<article>
 		    <a href="../vista/celulares.html" id="til"> <img  src="../imagenes/tit-cel.png" alt="Celulares"> </a>
 			    <ul>
@@ -339,35 +327,37 @@
 	       </article>
 	</section>
 	
+	
 
-    <!-- FOOTER DE LA PAGINA -->
+		
+		<!-- FOOTER DE LA PAGINA -->
 
  
     
     <footer>
     	<div class="box">
-	    <div class="redes">
-	       <p>Buscanos en</p><br>
-	          <div><a><img id="face" src="../imagenes/facebook.png">UNLIMITED TECNOLOGY</a></div><br>
-	          <div><a><img id="twit" src="../imagenes/twitter.png"> @UNLIMITED_TECNOLOGY</a></div><br>
-	          <div><a><img id="gml" src="../imagenes/instagram.png">unlimited.tec2021</a></div>
-	    </div>
+	        <div class="redes">
+	             <p>Buscanos en</p><br>
+	             <div><a><img id="face" src="../imagenes/facebook.png">UNLIMITED TECNOLOGY</a></div><br>
+	             <div><a><img id="twit" src="../imagenes/twitter.png"> @UNLIMITED_TECNOLOGY</a></div><br>
+	             <div><a><img id="gml" src="../imagenes/instagram.png">unlimited.tec2021</a></div>
+	        </div>
 
-	    <div class="contactenos">
-		      <div><img id="phon" src="../imagenes/Phone.png">Contactenos</div> <br>	
-			<ul>
-                            <li> Ingeniero de Sistemas: editoz@hotmail.com - editozz@gmail.com </li> <br>
-			    <li> Celular: 310 619 91 97 </li><br>
-			    <li> Servicios a domicilio de mantenimiento y soporte:<br><br>316 514 0646 – 301 366 7023 </li> <br>
-		            <img id="pin" src="../imagenes/pin.png">Calle 167 C No. 54C-36 Piso 3 - Bogotá D. C. - Colombia<br>
-		        </ul>
-		     </div>
+	        <div class="contactenos">
+		         <div><img id="phon" src="../imagenes/Phone.png">Contactenos</div> <br>	
+			     <ul>
+                     <li> Ingeniero de Sistemas: editoz@hotmail.com - editozz@gmail.com </li> <br>
+			         <li> Celular: 310 619 91 97 </li><br>
+			         <li> Servicios a domicilio de mantenimiento y soporte:<br><br>316 514 0646 – 301 366 7023 </li> <br>
+		             <img id="pin" src="../imagenes/pin.png">Calle 167 C No. 54C-36 Piso 3 - Bogotá D. C. - Colombia<br>
+		         </ul>
+		    </div>
 
-             <div class="terminos">
-		   <p><a href="../vista/terminos y condiciones.html">Terminos y Condiciones</a></p><br>
-		   <p id="copyright">| © 2021 |  Todos los derechos reservados | Unlimited Tecnhology</p>
-	      </div>
-	</div>
+            <div class="terminos">
+		         <p><a href="../vista/terminos y condiciones.html">Terminos y Condiciones</a></p><br>
+		         <p id="copyright">| © 2021 |  Todos los derechos reservados | Unlimited Tecnhology</p>
+	        </div>
+	 </div>
    </footer>
 
 </body>

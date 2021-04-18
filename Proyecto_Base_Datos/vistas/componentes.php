9e66158fbf30e71114e40db4e842d093262e5fca
Sara-mariana <76223831+Sara-mariana@users.noreply.github.com>
2021-04-18 17:59:16 -0500

    Update componentes.php
    reajuste de los links

--- Proyecto_Base_Datos/vistas/componentes.php
+++ Proyecto_Base_Datos/vistas/componentes.php
@@ -20,7 +20,6 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="keyswords" content="html5, css3">
-	<link rel="stylesheet" type="text/css" href="../css/estilos4.css">
 	<link rel="stylesheet" type="text/css" href="../css/menu_desplegable_filtro.css">
 	<script src="https://use.fontawesome.com/56837f1421.js"></script>
    <!-- <link rel="stylesheet" type="text/css" href="../css/icono.css">  -->   

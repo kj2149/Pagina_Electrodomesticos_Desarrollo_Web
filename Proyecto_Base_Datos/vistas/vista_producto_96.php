d4eb0edb74d70b4b30253422a92ef4b6e83101a1
dayana737 <76223627+dayana737@users.noreply.github.com>
2021-04-29 10:42:51 -0500

    Update vista_producto_96.php

--- Proyecto_Base_Datos/vistas/vista_producto_96.php
+++ Proyecto_Base_Datos/vistas/vista_producto_96.php
@@ -38,74 +38,75 @@
 
 <body id="parallax" class="text-white">
 
-<header class="container-head">
-
+<!--***********************-------   HEADER  -----------*************************************************-->
+	<header>
+           <div class="container-head">
+	  
+      
      <!--LOGO DE LA EMPRESA-->
 	  
-		<a href="../vista/index.html"class="logo"> <img  src="../imagenes/2.png" alt="logoempresa">	</a>
+		<a href="../vistas/index.php"class="logo"> <img  src="../imagenes/2.png" alt="logoempresa">	</a>
 	
      <!--TITULO ULIMITED TECNHOLOGY-->
 
-	 	<a href="../vista/index.html" class="titulo1"> <img  src="../imagenes/titulo.png" alt="UNLIMITED TECNHOLOGY"> </a>
+	 	<a href="../vistas/index.php" class="titulo1"> <img  src="../imagenes/titulo.png" alt="UNLIMITED TECNHOLOGY"> </a>
 
      <!--BUSCADOR-BOTONES INICIO , REGISTRAR-->
 
-           <div class="buscar1">
-               <div class=" barra-btn-buscar">
-                   <input type="text" placeholder="Buscar" required>
-                   <div class="btn">
-                      <i class="fas fa-search icon"></i>
-                   </div>
-                </div>
-                <a class="iniciar1" href="../vista/login.html" ><button type="button" class="bot-log-reg">Acceder</button></a> 
-                <a class="registrar1" href="#" > <button type="button" class="bot-log-reg">Registrarse</button></a>
-           </div>
+	 <div class="buscar1">
+			<div class=" barra-btn-buscar">
+			  <input type="text" placeholder="Buscar" required>
+			  <div class="btn">
+				<i class="fas fa-search icon"></i>
+			  </div>
+		      </div>
+		      <a class="iniciar1" href="../vistas/login.php" ><button type="button" class="bot-log-reg">Acceder</button></a> 
+		      <a class="registrar1" href="../vistas/registro.php" > <button type="button" class="bot-log-reg">Registrarse</button></a>
+        </div>
+    <!--Menu Header-->
+		<ul id="css3menu1" class="topmenu">
+			<li class="topmenu"><a href="index.php" target="marco" style="height:28px;line-height:22px;">Inicio</a>
+			</li>
+			<li class="toproot"><a href="#" target="marco"style="height:28px;line-height:22px;"><span>Productos</span></a>
+				<ul>
+					<li><a href="../vistas/celulares.php" target="marco">Celulares</a></li>
+					<li><a href="../vistas/computadores.php" target="marco">Computadores</a>
 
-     <!------MENU HEADER----->
- 
-       <ul id="css3menu1" class="topmenu">
-      <li class="topmenu"><a href="inicio.html" target="marco" style="height:28px;line-height:22px;">Inicio</a>
-      </li>
-      <li class="toproot"><a href="#" target="marco"style="height:28px;line-height:22px;"><span>Productos</span></a>
-        <ul>
-          <li><a href="../vista/celulares.html" target="marco">Celulares</a></li>
-          <li><a href="../vista/computadores.html" target="marco">Computadores</a>
-
-              <ul style="left:150px;">
-              
-              <li><a href="../vista/combo teclado + mouse.html">Combo teclado + Mouse</a></li>
-            <li><a href="../vista/portatiles.html">Portatiles</a></li>
-            <li><a href="../vista/equipos de escritorio.html">Equipos De Escritorio</a></li>
-            <li><a href="../vista/equipos todo en uno.html">Equipos Todo En Uno</a></li>
+					    <ul style="left:150px;">
+							
+					    <li><a href="../vistas/combo teclado + mouse.php">Combo teclado + Mouse</a></li>
+						<li><a href="../vistas/portatiles.php">Portatiles</a></li>
+						<li><a href="../vistas/equipos de escritorio.php">Equipos De Escritorio</a></li>
+						<li><a href="../vistas/equipos todo en uno.php">Equipos Todo En Uno</a></li>
                         </ul>
-            </li>
-        
-          <li><a href="../vista/boards.html" target="marco">Boards</a></li>
-          <li><a href="../vista/tarjetas graficas.html" target="marco">Tarjetas Graficas</a></li>
-          <li><a href="../vista/procesadores.html" target="marco">Procesadores</a></li>
-          <li><a href="../vistas/gabinetes.php" target="marco">Gabinetes</a></li>
-          <li><a href="../vistas/fuentes de poder.php" target="marco">Fuentes De Poder</a></li>
-          <li><a href="../vistas/impresoras.php" target="marco">Impresoras</a>
-          <li><a href="../vistas/herramientas.php" target="marco">herramientas</a>
-      
+						</li>
+				
+					<li><a href="../vistas/boards.php" target="marco">Boards</a></li>
+					<li><a href="../vistas/tarjetas graficas.php" target="marco">Tarjetas Graficas</a></li>
+					<li><a href="../vistas/procesadores.php" target="marco">Procesadores</a></li>
+					<li><a href="../vistas/gabinetes.php" target="marco">Gabinetes</a></li>
+					<li><a href="../vistas/fuentes de poder.php" target="marco">Fuentes De Poder</a></li>
+					<li><a href="../vistas/impresoras.php" target="marco">Impresoras</a>
+					<li><a href="../vistas/herramientas.php" target="marco">herramientas</a>
+			
 
-          <ul style="left:150px;">  
-              <li><a href="../vistas/componentes.php">Componentes</a></li>
-            <li><a href="../vistas/repuestos.php">Repuestos</a></li>
-            <li><a href="../vistas/cableado.php">Cableado</a></li>
+					<ul style="left:150px;">	
+					    <li><a href="../vistas/componentes.php">Componentes</a></li>
+						<li><a href="../vistas/repuestos.php">Repuestos</a></li>
+						<li><a href="../vistas/cableado.php">Cableado</a></li>
                         </ul>
-            </li>
-        </ul>
-      </li>
-      <li class="toproot"><a href="servicio.html" target="marco"style="height:28px;line-height:22px;"><span>Servicios</span></a></li>
-      
-      <li class="toproot"><a href="quienes somos.html" target="marco" style="height:28px;line-height:22px;"><span>Quienes somos?</span></a></li>
-      <li class="toplast"><a href="contactenos.html" target="marco" style="height:26px;line-height:22px;">Contactenos</a></li>
-    </ul>
-    
-     </div>
-  </header>
-
+						</li>
+				</ul>
+			</li>
+			<li class="toproot"><a href="servicio.php" target="marco"style="height:28px;line-height:22px;"><span>Servicios</span></a></li>
+			
+			<li class="toproot"><a href="quienes_somos.php" target="marco" style="height:28px;line-height:22px;"><span>Quienes somos?</span></a>
+			</li>
+			<li class="toplast"><a href="contactenos.php" target="marco" style="height:26px;line-height:22px;">Contactenos</a></li>
+		</ul>
+		
+	   </div>
+	</header>
 <!--**************************** FIN ---- HEADER    *************************************************-->
   <!--------------------------------presentacion del producto ----------------------------->
  <div class="especificartermino1">

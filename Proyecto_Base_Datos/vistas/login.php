17971795661609f4ab4f7acb4e538413a6c0e73b
dayana737 <76223627+dayana737@users.noreply.github.com>
2021-04-29 08:43:36 -0500

    Update login.php

--- Proyecto_Base_Datos/vistas/login.php
+++ Proyecto_Base_Datos/vistas/login.php
@@ -110,11 +110,12 @@
       
      <!--LOGO DE LA EMPRESA-->
 	  
-		<img class="logo" src="../imagenes/2.png" alt="logoempresa">	
+		<a href="../vistas/index.php"class="logo"> <img  src="../imagenes/2.png" alt="logoempresa">	</a>
 	
      <!--TITULO ULIMITED TECNHOLOGY-->
 
-		  <img class="titulo1" src="../imagenes/titulo.png" alt="UNLIMITED TECNHOLOGY">
+	 	<a href="../vistas/index.php" class="titulo1"> <img  src="../imagenes/titulo.png" alt="UNLIMITED TECNHOLOGY"> </a>
+
 
      <!--BUSCADOR-BOTONES INICIO , REGISTRAR-->
 
@@ -167,7 +168,7 @@
 			</li>
 			<li class="toproot"><a href="servicio.php" target="marco"style="height:28px;line-height:22px;"><span>Servicios</span></a></li>
 			
-			<li class="toproot"><a href="quienes somos.php" target="marco" style="height:28px;line-height:22px;"><span>Quienes somos?</span></a>
+			<li class="toproot"><a href="quienes_somos.php" target="marco" style="height:28px;line-height:22px;"><span>Quienes somos?</span></a>
 			</li>
 			<li class="toplast"><a href="contactenos.php" target="marco" style="height:26px;line-height:22px;">Contactenos</a></li>
 		</ul>

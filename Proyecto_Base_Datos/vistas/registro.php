aae94ff6591427e7e35f215e4b4cd4c33206ed97
dayana737 <76223627+dayana737@users.noreply.github.com>
2021-04-29 07:44:54 -0500

    Conexión a menú.

--- Proyecto_Base_Datos/vistas/registro.php
+++ Proyecto_Base_Datos/vistas/registro.php
@@ -120,18 +120,65 @@
 
 	 <div class="buscar1">
 			<div class=" barra-btn-buscar">
-			   <input type="" placeholder="Buscar" required>
-			   <div class="btn">
-				<i class="fas fa-search icon"></i>
-			  </div>
-		      </div>
-		     
+			   <input type="text" placeholder="Buscar" required>
+			    <div class="btn">
+				    <i class="fas fa-search icon"></i>
+			    </div>
+		    </div>
+		    <a class="iniciar1" href="../vistas/login.php" ><button type="button" class="bot-log-reg">Acceder</button></a> 
+		    <a class="registrar1" href="../vistas/registro.php" > <button type="button" class="bot-log-reg">Registrarse</button></a>
         </div>
 
 
+
+		<ul id="css3menu1" class="topmenu">
+			<li class="topmenu"><a href="../vistas/index.php" target="marco" style="height:28px;line-height:22px;">Inicio</a>
+			</li>
+			<li class="toproot"><a href="#" target="marco"style="height:28px;line-height:22px;"><span>Productos</span></a>
+				<ul>
+					<li><a href="../vistas/celulares.php" target="marco">Celulares</a></li>
+					<li><a href="../vistas/computadores.php" target="marco">Computadores</a>
+
+					    <ul style="left:150px;">
+							
+					    <li><a href="../vistas/combo teclado + mouse.php">Combo teclado + Mouse</a></li>
+						<li><a href="../vistas/portatiles.php">Portatiles</a></li>
+						<li><a href="../vistas/equipos de escritorio.php">Equipos De Escritorio</a></li>
+						<li><a href="../vistas/equipos todo en uno.php">Equipos Todo En Uno</a></li>
+                        </ul>
+						</li>
+				
+					<li><a href="../vistas/boards.php" target="marco">Boards</a></li>
+					<li><a href="../vistas/tarjetas graficas.php" target="marco">Tarjetas Graficas</a></li>
+					<li><a href="../vistas/procesadores.php" target="marco">Procesadores</a></li>
+					<li><a href="../vistas/gabinetes.php" target="marco">Gabinetes</a></li>
+					<li><a href="../vistas/fuentes de poder.php" target="marco">Fuentes De Poder</a></li>
+					<li><a href="../vistas/impresoras.php" target="marco">Impresoras</a>
+					<li><a href="../vistas/herramientas.php" target="marco">herramientas</a>
+			
+
+					<ul style="left:150px;">	
+					    <li><a href="../vistas/componentes.php">Componentes</a></li>
+						<li><a href="../vistas/repuestos.php">Repuestos</a></li>
+						<li><a href="../vistas/cableado.php">Cableado</a></li>
+                        </ul>
+						</li>
+				</ul>
+			</li>
+			<li class="toproot"><a href="servicio.php" target="marco"style="height:28px;line-height:22px;"><span>Servicios</span></a></li>
+			
+			<li class="toproot"><a href="quienes_somos.php" target="marco" style="height:28px;line-height:22px;"><span>Quienes somos?</span></a>
+				
+			<li class="toplast"><a href="contactenos.php" target="marco" style="height:26px;line-height:22px;">Contactenos</a></li>
+		</ul>
+		
+	   </div>
+	</header>
+
 		
 
-     <!--Menu-->
+     <!--Fin Header-->
+	<!--Menu-->
 	 
 		<ul id="css3menu1" class="topmenu">
 			<li class="topmenu"><a href="inicio.html" target="marco" style="height:28px;line-height:22px;">Inicio</a>
